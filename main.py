e6cea642d8be68fc1e9a774d6adb756e785a28d2
Saurabh Kumar <sauravkumar405@gmail.com>
2021-09-09 12:07:50 +0530

    Added loop in main.py

--- main.py
+++ main.py
@@ -4,22 +4,34 @@ import module2
 import module3
 import module4
 
-if __name__ == '__main__':
-    file_handle = open("DataSet/data.csv", "r")
+
+
+def fetchdata():
+    file_handle = open("/home/saurabh/Desktop/project/data-project-python/DataSet/data.csv", "r")
     DataSet = DictReader(file_handle)
+    return DataSet
+
+
+if __name__ == '__main__':
+    
     print("Enter 1 to view Population of India over Years")
     print("Enter 2 to view Bar Chart of population of ASEAN countries")
     print("Enter 3 to view BAR Chart of Total SAARC countries population vs \
         years")
     print("Enter 4 to view Grouped Bar Chart of ASEAN countries population \
         vs years")
-    choice = int(input("Enter your choice "))
-    if choice == 1:
-        module1.function(DataSet)
-    elif choice == 2:
-        module2.function(DataSet)
-    elif choice == 3:
-        module3.function(DataSet)
-    elif choice == 4:
-        module4.function(DataSet)
-    file_handle.close()
+    print("Enter 0 to exit")
+
+    while(True):
+        choice = int(input("Enter your choice "))
+        DataSet = fetchdata()
+        if choice == 1:
+            module1.function(DataSet)
+        elif choice == 2:
+            module2.function(DataSet)
+        elif choice == 3:
+            module3.function(DataSet)
+        elif choice == 4:
+            module4.function(DataSet)
+        else:
+            break
